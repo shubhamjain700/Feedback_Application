bb85a829e8ac8077f4ed6093608982e00c39a015
shubhamjain700 <shubham.jain_cs17@gla.ac.in>
2019-09-29 12:32:12 +0530

    Initial commit

---
+++ app/src/main/java/com/appsquadz/feedback_application/Splash.java
@@ -0,0 +1,26 @@
+package com.appsquadz.feedback_application;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    Handler handler=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Runnable runnable=new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,SignActivity.class));
+
+            }
+        };
+        handler.postDelayed(runnable,3000);
+
+    }
+}
