a78be6989c9aba40a286ada8272f08aa9b2dde32
shubhamjain700 <shubham.jain_cs17@gla.ac.in>
2019-10-16 22:56:33 +0530

    SignUp Form

--- app/src/main/java/com/appsquadz/feedback_application/SignUp.java
+++ app/src/main/java/com/appsquadz/feedback_application/SignUp.java
@@ -1,7 +1,10 @@
 package com.appsquadz.feedback_application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
 
@@ -9,6 +12,19 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        final TextView signin;
+
+
+        signin=findViewById(R.id.link_login);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(SignUp.this,SignActivity.class));
+            }
+        });
+
 
     }
 }
