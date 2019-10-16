977c4a78ff3d587b40228de50f36b47699c1e8a1
shubhamjain700 <shubham.jain_cs17@gla.ac.in>
2019-10-16 23:28:18 +0530

    FeedBackForm

---
+++ app/src/main/java/com/appsquadz/feedback_application/FeedBack_Form.java
@@ -0,0 +1,13 @@
+package com.appsquadz.feedback_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedBack_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_back__form);
+    }
+}
