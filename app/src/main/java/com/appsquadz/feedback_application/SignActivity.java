894a9eb28a3767ecc0791fe5d714d5f7f6f1035e
shubhamjain700 <shubham.jain_cs17@gla.ac.in>
2019-10-16 22:32:33 +0530

    link to SignUp

--- app/src/main/java/com/appsquadz/feedback_application/SignActivity.java
+++ app/src/main/java/com/appsquadz/feedback_application/SignActivity.java
@@ -1,13 +1,34 @@
 package com.appsquadz.feedback_application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignActivity extends AppCompatActivity {
+    TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+        signup=findViewById(R.id.sign_up);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+    startActivity(new Intent(SignActivity.this,SignUp.class));
+
+
+
+            }
+        });
+
+
+
+
+
+
     }
 }
