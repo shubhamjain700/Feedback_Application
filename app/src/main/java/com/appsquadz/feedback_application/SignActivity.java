1a9fd982a7f8dfc0fef9933c6aa20fd8d0865467
shubhamjain700 <shubham.jain_cs17@gla.ac.in>
2019-11-12 22:02:43 +0530

    FeedBackForm firebase

--- app/src/main/java/com/appsquadz/feedback_application/SignActivity.java
+++ app/src/main/java/com/appsquadz/feedback_application/SignActivity.java
@@ -1,26 +1,60 @@
 package com.appsquadz.feedback_application;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignActivity extends AppCompatActivity {
     TextView signup;
+    FirebaseAuth firebaseAuth;
+    EditText email;
+    EditText passowrd;
+    Button login;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
         signup=findViewById(R.id.sign_up);
+        firebaseAuth=FirebaseAuth.getInstance();
+        login=findViewById(R.id.login1);
+        email=findViewById(R.id.email);
+        passowrd=findViewById(R.id.password);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String e=email.getText().toString().trim();
+                String pass=passowrd.getText().toString().trim();
+                validation(e,pass);
+//                firebaseAuth.signInWithEmailAndPassword(e,pass);
+//
+
+            }
+        });
+
+
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
     startActivity(new Intent(SignActivity.this,SignUp.class));
-
-
 
             }
         });
@@ -31,4 +65,29 @@ public class SignActivity extends AppCompatActivity {
 
 
     }
+
+    private void validation(String e, String pass) {
+
+
+
+        firebaseAuth.signInWithEmailAndPassword(e,pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            startActivity(new Intent(SignActivity.this,FeedBack_Form.class));
+
+                            Toast.makeText(SignActivity.this,"Sucessful",
+                                    Toast.LENGTH_LONG).show();
+
+                        }
+                        else {
+                            Toast.makeText(SignActivity.this,""+task.isSuccessful(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    });
+    }
+
 }
--- app/src/main/java/com/appsquadz/feedback_application/SignUp.java
+++ app/src/main/java/com/appsquadz/feedback_application/SignUp.java
@@ -1,25 +1,40 @@
 package com.appsquadz.feedback_application;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUp extends AppCompatActivity {
+
+    EditText email;
+    EditText password;
+    FirebaseAuth firebaseAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         final TextView signin;
+        firebaseAuth=FirebaseAuth.getInstance();
 
-
+        email=findViewById(R.id.input_email);
         signin=findViewById(R.id.link_login);
+        password=findViewById(R.id.input_password);
 
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String em=email.getText().toString().trim();
+                String pass=password.getText().toString().trim();
+
+                firebaseAuth.createUserWithEmailAndPassword(em,pass);
 
                 startActivity(new Intent(SignUp.this,SignActivity.class));
             }
